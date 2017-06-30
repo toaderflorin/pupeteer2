f377b11def5f8abc40500f1b911610fe625b773c
toaderflorin <toaderflorin@gmail.com>
2017-06-30 20:12:20 +0300

    Added support for groups.

--- pupeteer.js
+++ pupeteer.js
@@ -1,9 +1,12 @@
+var allGroups = [];
 var allElements = [];
+var allGroupElements = [];
+var allElementsSet = new Set();
 var windowOffset = 0;
 var scrollUp = false;
 
 var update = function () {
-  allElements.forEach(function (e) {
+  allElementsSet.forEach(function (e) {
     var rect = e.getBoundingClientRect();
     var anim = e.getAttribute('p-animation');
     var delay = e.getAttribute('p-delay');
@@ -14,19 +17,63 @@ var update = function () {
           e.classList.add(anim);
         }, delay);
       } else {
-         window.setTimeout(function () {
+        window.setTimeout(function () {
           e.classList.add(anim);
         }, 0);
       }
-     
+
     } else if (rect.top > window.innerHeight) {
       e.classList.remove(anim);
+    }
+  });
+
+  allGroups.forEach(function (g) {
+    var rect = g.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight && rect.top > 0 && !scrollUp) {
+      allElements.forEach(function (e) {
+        var anim = e.getAttribute('p-animation');
+        var delay = e.getAttribute('p-delay');
+        if (delay) {
+          window.setTimeout(function () {
+            e.classList.add(anim);
+          }, delay);
+        } else {
+          window.setTimeout(function () {
+            e.classList.add(anim);
+          }, 0);
+        }
+      });
+    } else if (rect.top > window.innerHeight) {
+      allElements.forEach(function (e) {
+        var anim = e.getAttribute('p-animation');
+        e.classList.remove(anim);
+      });
     }
   });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   allElements = document.querySelectorAll('[p-animation]');
+  allElements.forEach(function (e) {
+    allElementsSet.add(e);
+  });
+
+  allGroups = document.querySelectorAll('[p-group]');
+
+  allGroups.forEach(function (grp) {
+    var groupName = grp.getAttribute('p-group');
+    console.log('GROUP=', groupName);
+    var elements = document.querySelectorAll(`[p-group="${groupName}"] [p-animation]`);
+    console.log('ELS:', elements);
+
+    allGroupElements.push(elements);
+    elements.forEach(function (e) {
+
+      allElementsSet.delete(e);
+    });
+  });
+
   update();
 });
 
